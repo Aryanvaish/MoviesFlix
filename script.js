66e4b598df8c9f99560fff4b5eaaa47442051b99
Aryanvaish <108726947+Aryanvaish@users.noreply.github.com>
2025-01-27 15:52:07 +0530

    bug fixes..

--- script.js
+++ script.js
@@ -31,12 +31,13 @@ fetchGenreData(`${baseUrl}/${apiVersion}/genre/tv/list?api_key=${ApiKey}`);
 
 
 export async function layoutRender(API_URL, parentDiv, secTitle) {
+
     const mainBox = document.querySelector(`.${parentDiv}`);
 
     mainBox.innerHTML = `<div class="titlePage">
                     <h2>${secTitle}</h2>
                     <div class="pages">
-                    <svg width="18px" class="prevPage prev-${parentDiv}"  height="17px" viewBox="0 0 18 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <g id="prev" transform="translate(8.500000, 8.500000) scale(-1, 1) translate(-8.500000, -8.500000)"> <polygon class="arrow" points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon> <polygon class="arrow-fixed" points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon> <path d="M-1.48029737e-15,0.56157424 L-1.48029737e-15,16.1929159 L9.708,8.33860465 L-2.66453526e-15,0.56157424 L-1.48029737e-15,0.56157424 Z M1.33333333,3.30246869 L7.62533333,8.34246869 L1.33333333,13.4327013 L1.33333333,3.30246869 L1.33333333,3.30246869 Z"></path></g> </svg>
+                    <svg width="18px" class="prevPage prev-${parentDiv}" height="17px" viewBox="0 0 18 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <g id="prev" transform="translate(8.500000, 8.500000) scale(-1, 1) translate(-8.500000, -8.500000)"> <polygon class="arrow" points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon> <polygon class="arrow-fixed" points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon> <path d="M-1.48029737e-15,0.56157424 L-1.48029737e-15,16.1929159 L9.708,8.33860465 L-2.66453526e-15,0.56157424 L-1.48029737e-15,0.56157424 Z M1.33333333,3.30246869 L7.62533333,8.34246869 L1.33333333,13.4327013 L1.33333333,3.30246869 L1.33333333,3.30246869 Z"></path></g> </svg>
                     <strong class="currPage curr-${parentDiv}">1</strong>
                     <svg class="nextPage next-${parentDiv}" data-actionPage=" width="18px" height="17px" viewBox="-1 0 18 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><polygon class="arrow" points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon><polygon class="arrow-fixed" points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"></polygon><path d="M-4.58892184e-16,0.56157424 L-4.58892184e-16,16.1929159 L9.708,8.33860465 L-1.64313008e-15,0.56157424 L-4.58892184e-16,0.56157424 Z M1.33333333,3.30246869 L7.62533333,8.34246869 L1.33333333,13.4327013 L1.33333333,3.30246869 L1.33333333,3.30246869 Z"></path></g></svg>
                     </div>
@@ -79,8 +80,6 @@ export async function UrlCreate(pageurl, parentDiv, page) {
     await fetch(`${pageurl}&page=${page}`)
         .then((response) => response.json())
         .then((data) => {
-
-
             if (data.results?.length === 0) {
                 document.querySelector('.searchResult ul').innerHTML = `<div class="notFound">Not Found, Please Try Agian !!!</div>`
             }
@@ -106,17 +105,16 @@ export async function UrlCreate(pageurl, parentDiv, page) {
                 let contentId = data.results[i].id;
                 let mediaType = data.results[i].media_type;
 
-                var genretags = "";
+                let genretags = "";
 
                 for (let j = 0; j < genreKey?.length; j++) {
-                    if (genres[genreKey[j]] == undefined) { return false; }
-                    genretags += `${genres[genreKey[j]]} ${j !== (genreKey.length - 1) ? ',' : ''} `;
+                    if (genres[genreKey[j]]) { 
+                        genretags += `${genres[genreKey[j]]} ${j !== (genreKey.length - 1) ? ',' : ''} `;
+                    }
                 }
 
-                // console.log(genretags);
-
                 const ratingStat = parseInt(rating);
-                var ratingBg = "";
+                let ratingBg = "";
 
                 if (ratingStat >= 8) {
                     ratingBg = "green";
@@ -126,9 +124,7 @@ export async function UrlCreate(pageurl, parentDiv, page) {
                     ratingBg = "red";
                 }
 
-
                 if (ratingStat == 0) { rating = "Upcoming"; }
-
                 listWrap.innerHTML += `   
                     <li class="cont_boxes">
                     <strong class="rating ${ratingBg}">${rating}</strong>  
@@ -145,9 +141,7 @@ export async function UrlCreate(pageurl, parentDiv, page) {
                     </div>
                     </li>
                     `;
-
             }
-
 
             if (!listWrap.hasChildNodes() || listWrap.innerHTML == "") {
                 listWrap.innerHTML = `<strong class="nodata">Coming Soon !!!</strong>`;
@@ -159,9 +153,9 @@ export async function UrlCreate(pageurl, parentDiv, page) {
                     moviePoster[j].setAttribute("src", "images/error.webp");
                 });
             }
-
-        })
-
+        }).catch((error) => {
+            console.error('Error:', error);
+        });
 }
 
 Promise.all([
@@ -170,12 +164,11 @@ Promise.all([
     layoutRender(`${baseUrl}/${apiVersion}/trending/tv/day?api_key=${ApiKey}`, "trendingTv", "Trending Series"),
     layoutRender(`${baseUrl}/${apiVersion}/movie/top_rated?api_key=${ApiKey}`, "toplistedMovies", "Top Listed Movies"),
     layoutRender(`${baseUrl}/${apiVersion}/tv/top_rated?api_key=${ApiKey}`, "toplistedSeries", "Top Listed Series"),
-]).then(() => {detailpopup()});
+]).then(() => { detailpopup() });
 
 export function popupClose() {
     popup_Warp.classList.add("hide");
     popup_Warp.innerHTML = "";
     document.querySelector('body').style.overflowY = "auto";
 }
-
 
